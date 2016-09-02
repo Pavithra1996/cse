b047ffc5583078348f6463ca67589262047b4821
Pavithra1996 <pavisindhu02@gmail.com>
2016-09-02 19:04:25 +0530

    Create fibo.c

---
+++ fibo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int f1,f2,f3,i;
+printf("Enter the no");
+scanf("%d",&n);
+f1=0;
+f2=1;
+for(i=0;i<=n;i++)
+f3=f1+f2;
+printf("%d",f3);
+return 0;
+}
