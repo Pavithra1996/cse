937d5899454b5bd404fda416b92d7ba61ca25fbd
Pavithra1996 <pavisindhu02@gmail.com>
2016-09-02 18:51:40 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("Enter the no of elements to be added");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+printf("enter the no to be added");
+scanf("%d",&a);
+sum=sum+a;
+return 0;
+}
